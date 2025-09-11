4c6c8650423387058f2d920273d10483ffb230ad
Ruben Ooms <ruooms@gmail.com>
2025-09-11 09:05:24 -0500

    Removing Debug and Deprecated Code

--- Script.py
+++ Script.py
@@ -17,8 +17,7 @@ class Spice:
         self.ascPath=ascPath
         self.spicePath=spicePath
 
-        # Automatically get .raw and .net paths
-        #self.rawPath="."+(self.ascPath.split(".")[1])+"_1.raw"
+        # Automatically get .net paths
         self.netPath="."+(self.ascPath.split(".")[1])+".net"
 
         # Create the simulator and netlist objects
@@ -26,41 +25,12 @@ class Spice:
         self.LTC.create_netlist(self.ascPath)
         self.netlist=SpiceEditor(self.netPath)
 
+        # Define callback processor
         self.callbackProc=callbackProc
 
     def simulate(self,runNetlistFile):
-        # Delete old file to ensure we dont get old data
-        #if os.path.exists(self.rawPath):
-            #os.remove(self.rawPath)
-        #if os.path.exists(self.netPath):
-            #os.remove(self.netPath)
-        #logPath="."+(self.rawPath.split(".")[1])+".log"
-        #opRawPath="."+(self.rawPath.split(".")[1])+".op.raw"
-        #if os.path.exists(logPath):
-            #os.remove(logPath)
-        #if os.path.exists(opRawPath):
-            #os.remove(opRawPath)
-
         # Run LTSpice simulation
         self.LTC.run(self.netlist,run_filename=runNetlistFile,callback=self.callbackProc)
-
-        # Make sure simulation has finished writing to .raw
-        #self.waitRaw()
-        #self.LTR=RawRead(self.rawPath)
-
-    def waitRaw(self):
-        # Ensure that the file exists before continuing
-        start=time.time()
-        while not os.path.exists(self.rawPath):
-            if time.time()-start>20:
-                raise TimeoutError("Could not open "+self.rawPath)
-
-        # Make sure the data has finished writing
-        size=os.path.getsize(self.rawPath)
-        while size==0:
-            size=os.path.getsize(self.rawPath)
-        while os.path.getsize(self.rawPath)>size:
-            size=os.path.getsize(self.rawPath)
 
     def getData(self,trace):
         if os.path.exists(self.rawPath):
@@ -83,12 +53,6 @@ class Optimizer:
         self.shelfs=shelfs
         self.component=tuple(self.getComponentNames())
 
-        # Get frequency data
-        #self.spice.simulate()
-        #self.freqx=np.array(self.spice.getData("frequency")).real.astype(float)
-        #self.freqx=np.log2(self.freqx).flatten().reshape(-1,1) # flatten reshape may not be necessary
-
-
     @staticmethod
     def tupleFromFile(filepath):
         # Read the entire file content
@@ -109,8 +73,6 @@ class Optimizer:
         return components
 
     def beginOptimization(self):
-        #self.spice.setVal("R1",15e3)
-
         bestError=50 # arbitrary
         comp=self.component[0]
         for r in self.rs:
@@ -126,34 +88,6 @@ class Optimizer:
 
         vout=self.spice.getData("V(vo)")
         return vout,self.freq
-
-    def iteration(self,n,errorBest,rc):
-        comp1=self.component[n*2+1]
-        comp2=self.component[n*2+2]
-        bestError=errorBest
-
-        for r in self.rs:
-            print(f"Optimizing: {comp1} {r}")
-            self.spice.setVal(comp1,r)
-            for c in self.cs:
-                print(f"Optimizing: {comp2} {c}")
-                if r*c>rc:
-                    continue
-                self.spice.setVal(comp2,c)
-                print(n,self.shelfs)
-                if n!=self.shelfs-1:
-                    error,vals=self.iteration(n+1,bestError,r*c)
-                    if error<bestError:
-                        bestError=error
-                        bestVals=[r,c]+vals
-                else:
-                    self.spice.simulate()
-                    for rawFile in self.spice.LTC:
-                        error=self.calcError(rawFile)
-                    if error<bestError:
-                        bestError=error
-                        bestVals=[r,c]
-                    return bestError,bestVals
 
     def generateRaws(self):
         self.cleanBatchDir()
@@ -206,14 +140,8 @@ class Optimizer:
                                                     self.spice.netlist.set_component_value("C6",c6)
                                                     runNetlistFile="{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}".format((self.spice.netlist.netlist_file.name).split(".")[0],r1,r2,c2,r3,c3,r4,c4,r5,c5,r6,c6)
                                                     self.spice.simulate(runNetlistFile)
-                                                    rawPath=os.path.join(batchDir,runNetlistFile+".raw")
-                                                    #self.spice.LTC.wait_completion()
-                                                    #if not os.path.exists(rawPath):
-                                                        #print(f"Missing raw file {rawPath}")
-
         self.spice.LTC.wait_completion()
         print("Successful/Total Simulations: " + str(self.spice.LTC.okSim) + "/" + str(self.spice.LTC.runno))
-        #self.deleteExtraFiles()
 
     def cleanBatchDir(self):
         scriptDir=os.path.dirname(os.path.abspath(__file__))
@@ -225,28 +153,6 @@ class Optimizer:
                     os.remove(itemPath)
                 elif os.path.isdir(itemPath):
                     shutil.rmtree(itemPath)
-
-    def deleteExtraFiles(self):
-        scriptDir=os.path.dirname(os.path.abspath(__file__))
-        batchDir=os.path.join(scriptDir,"batch")
-        if os.path.isdir(batchDir):
-            for item in os.listdir(batchDir):
-                if item.endswith(".raw") and not item.endswith(".op.raw"):
-                    continue
-                if item.endswith(".op.raw") or item.endswith(".log") or item.endswith(".net"):
-                    itemPath=os.path.join(batchDir,item)
-                    try:
-                        os.remove(itemPath)
-                    except:
-                        print(f"Error deleteing {itemPath}")
-                    continue
-                end=item.split("_")[-1]
-                if end.isdigit():
-                    itemPath=os.path.join(batchDir,item)
-                    try:
-                        os.remove(itemPath)
-                    except:
-                        print(f"Error deleteing {itemPath}")
 
     def findBest(self):
         freq=self.getFrequencyData()
@@ -261,16 +167,8 @@ class Optimizer:
         spice=Spice(self.spice.ascPath,self.spice.spicePath,"./",callbacks.CallbackProcFreq)
         runNetlistFile="{}_{}".format((spice.netlist.netlist_file.name).split(".")[0],"Frequency")
         spice.simulate(runNetlistFile)
-
-        #rawPath=runNetlistFile.split(".")[0]+".raw"
-        #logPath=runNetlistFile.split(".")[0]+".log"
-        #print(rawPath,logPath)
-        #return callbacks.CallbackProcFreq.callback(rawPath,logPath)
-
         for freq in spice.LTC:
             return freq
-
-
 
     def calcError(self,rawFile):
         self.LTR=RawRead(rawFile)
@@ -290,29 +188,6 @@ class Optimizer:
         m=10*np.log10(1/2)
         error=(slope/m-1)**2
         return error
-
-#class CallbackProcVO(ProcessCallback):
-#    @staticmethod
-#    def callback(rawFile,_):
-#        rawData=RawRead(rawFile)
-#        #freq_trace = rawData.get_trace("frequency")
-#        #freq = np.array(freq_trace.get_wave()).real.astype(float)
-#        #freq = np.log2(freq).flatten()
-#        vout_trace = rawData.get_trace("V(vo)")
-#        vout = np.array(vout_trace.get_wave()).flatten()
-#        return vout
-
-#class CallbackProcFreq(ProcessCallback):
-#    @staticmethod
-#    def callback(rawFile,_):
-#        rawData=RawRead(rawFile)
-#        freq_trace = rawData.get_trace("frequency")
-#        freq = np.array(freq_trace.get_wave()).real.astype(float)
-#        freq = np.log2(freq).flatten()
-#        return freq
-
-        
-
 
 def infoOut(differences,slope,m,r2):
     # Prints various pieces of information
@@ -353,7 +228,6 @@ def main():
     folder="./batch"
     callbackProc=callbacks.CallbackProcVO
     spice=Spice(ascPath,spicePath,folder,callbackProc)
-    #spice.simulate()
 
     # Path to text file with common sizes
     rPath=r".\common_resistor_sizes.txt"
@@ -361,7 +235,6 @@ def main():
 
     # Create optimizer object
     opt=Optimizer(spice,5,cPath,rPath)
-    #vout,freq=opt.beginOptimization()
     opt.generateRaws()
     opt.findBest()
     return
